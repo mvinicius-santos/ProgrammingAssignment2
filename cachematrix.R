8cf5c6fa3a499a1fb5c01ddc3abdb7b8315f0a8e
Marcus Santos <mvinicius.santos@gmail.com>
2015-03-17 13:13:00 -0300

    ProgrammingAssingment2-cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -12,7 +12,7 @@ makeCacheMatrix <- function(x = matrix()) {
     m <<- NULL
   }
   get <- function() x
-  setinverse <- function(solve) m <<- solve #mean
+  setinverse <- function(solve) m <<- solve
   getinverse <- function() m
   list(set = set, get = get,
        setinverse = setinverse,
